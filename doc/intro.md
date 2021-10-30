8c32744ab4ba6b292ba7fdf7fe759b8e78140739
dviramontes <dviramontes@gmail.com>
2021-10-30 13:57:34 -0600

    project init via seancorfield's clj-new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gh-release-monitor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dviramontes/gh_release_monitor_test.clj
@@ -0,0 +1,7 @@
+(ns dviramontes.gh-release-monitor-test
+  (:require [clojure.test :refer :all]
+            [dviramontes.gh-release-monitor :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
